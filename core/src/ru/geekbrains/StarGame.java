0312c50be3d919c219fee5095d32585a3f1d63c0
AntonKravchenko <kravchenko_ae@mail.ru>
2021-02-10 16:13:00 +0300

    Code from classwork

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -1,53 +1,14 @@
 package ru.geekbrains;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.PolygonRegion;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.Game;
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	Texture background;
+import ru.geekbrains.screen.MenuScreen;
 
-	int x, y, screenWidth, screenHeight, dirX, dirY;
+public class StarGame extends Game {
 
-	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		background = new Texture("black3.jpg");
-		screenWidth = Gdx.graphics.getWidth();
-		screenHeight = Gdx.graphics.getHeight();
-		dirX = 1;
-		dirY = 1;
-
-
+		setScreen(new MenuScreen());
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		x += dirX;
-		y += dirY;
-		if ((x > (screenWidth - img.getWidth())) || (x < 0)) dirX = -dirX;
-		if ((y > (screenHeight - img.getHeight())) || (y < 0)) dirY = -dirY;
-		batch.begin();
-		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(img, x, y);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
 }
